b77af08bd8a7b29f6785f3b9837b4a032b01fa85
zhangjingqiang <zhangjingqiang@live.com>
2015-01-20 14:10:38 +0000

    Fix pagination bug for big category select

--- app/controllers/big_categories_controller.rb
+++ app/controllers/big_categories_controller.rb
@@ -1,6 +1,6 @@
 class BigCategoriesController < ApplicationController
   def show
     @big_categories = BigCategory.all
-    @small_categories = SmallCategory.where(big_category_id: params[:id])
+    @small_categories = SmallCategory.where(big_category_id: params[:id]).paginate(:page => params[:page])
   end
 end
