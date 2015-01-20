452e7143a7a54a2aec763dee4710fb732548773d
zhangjingqiang <zhangjingqiang@live.com>
2015-01-20 14:06:07 +0000

    Add pagination to small categories

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     @big_categories = BigCategory.all
-    @small_categories = SmallCategory.where(big_category_id: @big_categories.map.with_index { |big, index| big.id if index == 0 })
+    @small_categories = SmallCategory.where(big_category_id: @big_categories.map.with_index { |big, index| big.id if index == 0 }).paginate(:page => params[:page])
   end
 end
