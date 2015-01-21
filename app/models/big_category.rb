acd23315b30bc0f266ea893ab03f09db8269debe
zhangjingqiang <zhangjingqiang@live.com>
2015-01-21 02:41:41 +0000

    Add uniqueness to big_category name and site url

--- app/models/big_category.rb
+++ app/models/big_category.rb
@@ -1,7 +1,7 @@
 class BigCategory < ActiveRecord::Base
   has_many :small_categories
   
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   
   default_scope { order('name') }
 end
--- app/models/site.rb
+++ app/models/site.rb
@@ -2,7 +2,7 @@ class Site < ActiveRecord::Base
   belongs_to :small_category
   
   validates :name, presence: true
-  validates :url, presence: true
+  validates :url, presence: true, uniqueness: true
   validates :big_category_id, presence: true
   validates :small_category_id, presence: true
   
